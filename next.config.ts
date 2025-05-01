ffe63f2f50ee209192096016ff2554c143d97325
William Will <10997562+willwill96@users.noreply.github.com>
2025-04-30 20:05:44 -0600

    Ensure that source maps are generated via next build

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack: (config) => {
+    config.devtool = 'hidden-source-map'
+    return config
+  }
 };
 
 export default nextConfig;
